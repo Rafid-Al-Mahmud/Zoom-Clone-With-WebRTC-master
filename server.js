b09d67c589c6a019888e530dd06748db7d0fce7a
Rafid-Al-Mahmud <76583107+Rafid-Al-Mahmud@users.noreply.github.com>
2023-03-03 15:03:40 +0600

    Update server.js

--- server.js
+++ server.js
@@ -26,4 +26,4 @@ io.on('connection', socket => {
   })
 })
 
-server.listen(3000,()=>console.log('SERVER IS STARTED AT http://localhost:3000/'))
+server.listen(process.env.PORT || 3000,()=>console.log('SERVER IS STARTED AT http://localhost:3000/'))
